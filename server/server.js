3c94748e02870a546103cafbf418d80863ae2103
Yuzinn <itsforme91@gmail.com>
2017-08-20 10:06:49 +0900

    add delete route

--- server/server.js
+++ server/server.js
@@ -42,8 +42,22 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo});
-  }).catch((e) => res.status(400).send(e));
+    res.status(200).send({todo});
+  }).catch((e) => res.status(400).send());
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
